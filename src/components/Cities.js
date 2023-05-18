098a4db21d7ffe76e699a4de3df68e4fdd8b8ffa
Vishal Suman <119152809+vishal3156@users.noreply.github.com>
2023-05-18 11:53:16 +0530

    Update Cities.js

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -1,22 +1,30 @@
-import React,{useState} from 'react';
-import Town from './Town';
+import React, { Component, useState } from "react";
+import Town from "./Town";
 
-function Cities({name,towns,index}) {
-    const [clicked, setClicked] = useState(false);
-    return (
-        <li>
-            <h4 id={'city'+(index+1)} onClick={()=>setClicked(!clicked)}>{name}</h4>
-            {clicked ? (
-                <ul>
-                {towns.map(item=>(
-                    <Town 
-                    name={item.name} 
-                    index={towns.indexOf(item)} />
-                ))}
-                </ul>     
-            ):null}
-        </li>
-    );
+function City(props) {
+  const [showTowns, setTowns] = useState(false);
+
+  function show() {
+    return props.cityVar.towns.map((townName, townId) => {
+      return <Town townName={townName} townId={townId} key={townId} />;
+    });
+  }
+
+  return (
+    <>
+      <div
+        id={"city" + (props.cityId + 1)}
+        onClick={() => {
+          setTowns(!showTowns);
+        }}
+      >
+        {props.cityName}
+      </div>
+      <div>
+        {showTowns ? show() : ""}
+      </div>
+    </>
+  );
 }
 
-export default Cities;
+export default City;
