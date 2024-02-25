43d30c1b5ea3df74a0d2bd4bbee003f14866d9d4
Vishal Suman <vishal21suman@outlook.com>
2024-02-25 17:27:45 +0530

    Update Town.js

--- src/components/Town.js
+++ src/components/Town.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function Town(props) {
-  return <div id={"town" + (props.townId + 1)}>{props.townName.name}</div>;
+  return <div id={"town" + (props.townId + 1)} > {props.townName.name}</div>;
 }
 
 export default Town;
